a2a8a451afe1755e42a475eb4e44da162a249581
LDF <liudengfeng_sd@outlook.com>
2024-07-28 13:44:38 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div> */}
+      <div className="container">
+        <div className="sidebar">
+          <h2>菜单</h2>
+          <ul>
+            <li>主页</li>
+            <li>探索</li>
+            <li>通知</li>
+            <li>消息</li>
+            <li>书签</li>
+            <li>列表</li>
+            <li>个人资料</li>
+            <li>更多</li>
+          </ul>
+        </div>
+        <div className="content">
+          <h2>内容</h2>
+          <p>这里是主要内容区域。</p>
+        </div>
+        <div className="widgets">
+          <h2>参数</h2>
+          <p>这里是参数区域。</p>
+        </div>
+      </div>
+    </>
   );
 }
 
